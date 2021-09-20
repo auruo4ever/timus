8fa48178dff7b3498f36cfcf78ab9a2f7052cc4b
Alisa <alica.2001@mail.ru>
2021-09-20 17:53:16 +0300

    first commit

---
+++ 1009/1009/1009.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int mas[20];
+	int n, k;
+	cin >> n >> k;
+	int num = k - 1; //все цифры данной системы счисления без 0
+	mas[0] = 1;
+	mas[1] = num;
+
+	for (int i = 2; i <= n; i++) {
+		mas[i] = (mas[i - 1] * num + mas[i - 2] * num); //отдельно когда добавляем 0 вариантов и не 0 вариантов
+	}
+	cout << mas[n];
+	return 0;
+}
